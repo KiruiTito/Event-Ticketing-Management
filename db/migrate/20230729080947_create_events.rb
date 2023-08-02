931354338737b288d1ff596e94faaedb3f9f4925
celinechege <chegeceline18@gmail.com>
2023-08-02 21:29:58 +0300

    add new migration

--- db/migrate/20230729080947_create_events.rb
+++ db/migrate/20230729080947_create_events.rb
@@ -7,7 +7,9 @@ class CreateEvents < ActiveRecord::Migration[7.0]
       t.string :category
       t.string :start_date
       t.string :end_date
-      t.integer :tickets_available
+      t.integer :vip_tickets_available
+      t.integer :regular_tickets_available
+
 
       t.timestamps
     end
