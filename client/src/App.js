e2f3ae963651b0366f7d1614ebf8c51fe6d7ea17
celinechege <chegeceline18@gmail.com>
2023-08-02 21:27:10 +0300

    add app js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,20 @@
-
+// App.js
+import React from 'react';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Main from './components/Main';
+// import Ticket from './components/Ticket';
 import './App.css';
-
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Main />
+    // <Router>
+    //   <Routes>
+    //     {/* Define your routes */}
+    //     <Route path="/" element={<Main />} />
+    //     <Route path="/tickets/:eventId" element={<Ticket/>} />
+    //   </Routes>
+    // </Router>
   );
 }
 
